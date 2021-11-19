e46b56518a2d0585d4eca23c5843685c10a26b63
acaldart <caldart123@gmail.com>
2021-11-18 21:42:10 -0300

    arreglos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from 'react';
+import Preloader from '../src/components/Pre';
+import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer';
+import Certs from './components/Cert/Certs';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './style.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from './components/ScrollToTop';
+import Resume from './components/Resume/Resume';
 
 function App() {
   const [load, upadateLoad] = useState(true);
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
-    
+
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <Router>
       <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div className='App' id={load ? 'no-scroll' : 'scroll'}>
         <Navbar />
         <ScrollToTop />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
-          <Route path="/about" component={About} />
-          <Route path="/resume" component={Resume} />
+          <Route path='/' exact component={Home} />
+          <Route path='/project' component={Projects} />
+          <Route path='/about' component={About} />
+          <Route path='/resume' component={Resume} />
+          <Route path='/cert' component={Certs} />
         </Switch>
         <Footer />
       </div>
---
+++ src/components/Cert/Certs.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import CertCard from './CertCard';
+import Particle from '../Particle';
+
+import apiNode from '../../Assets/Cert/apiNodeJS.png';
+import arrayJs from '../../Assets/Cert/arrayJs.png';
+import asincronismoJs from '../../Assets/Cert/asincronismoJs.png';
+import basicoJS from '../../Assets/Cert/basicoJS.png';
+import cloudAWS from '../../Assets/Cert/cloudAWS.png';
+import cloudComputingAWS from '../../Assets/Cert/cloudComputingAWS.png';
+import db from '../../Assets/Cert/db.png';
+import ecmaJS from '../../Assets/Cert/ecmaJS.png';
+import fundamentoJS from '../../Assets/Cert/fundamentoJS.png';
+import go from '../../Assets/Cert/go.png';
+import npm from '../../Assets/Cert/npm.png';
+import PooGO from '../../Assets/Cert/PooGO.png';
+import prework from '../../Assets/Cert/prework.png';
+import reactHook from '../../Assets/Cert/reactHook.png';
+import reactRouterRedux from '../../Assets/Cert/reactRouterRedux.png';
+import scopeJS from '../../Assets/Cert/scopeJS.png';
+import storageAWS from '../../Assets/Cert/storageAWS.png';
+import v8JS from '../../Assets/Cert/v8JS.png';
+
+function Certs() {
+  return (
+    <Container fluid className='project-section'>
+      <Particle />
+      <Container>
+        <h1 className='project-heading'>
+          Mis <strong className='purple'>Certificados </strong>
+        </h1>
+        <p style={{ color: 'white' }}>Aca se encuentran mis certificaciones</p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={prework}
+              title='PreWork'
+              description='Configuración WSL2'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={v8JS}
+              title='V8 JS'
+              description='Historia y lógica del motor v8'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={basicoJS}
+              title='Básico JS'
+              description='Conceptos básicos de JS'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={ecmaJS}
+              title='EcmaScript 6+'
+              description='Historia de ecma y cambios en el tiempo'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={asincronismoJs}
+              title='Asincronismo JavaScript'
+              description='Manejo de asincronismo con Callback,Promise y Async-Await'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={fundamentoJS}
+              title='Fundamentos de JavaScript'
+              description='Fundamentos de JS'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={scopeJS}
+              title='Scope JavaScript'
+              description='Scope/Hoisting/Clousure'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={arrayJs}
+              title='Array en JavaScript'
+              description='Manejo de array en Javascript'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={npm}
+              title='NPM'
+              description='Manejo y conceptos de NPM'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={db}
+              title='Fundamentos base de datos'
+              description='Fundamentos de las base de datos relacionadas y no relacionadas (MySQL/Firebase)'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={cloudAWS}
+              title='Cloud Aws'
+              description='Conceptos de programación en la nube'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={cloudComputingAWS}
+              title='Computing Aws'
+              description='Diferentes formas para hostear una aplicación en AWS'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={storageAWS}
+              title='Storage AWS'
+              description='Persistencia de datos con buckets'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={go}
+              title='Golang'
+              description='Primer vistaso a Golang'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={PooGO}
+              title='Poo Golang'
+              description='Programación orientada a objetos con Golang'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={apiNode}
+              title='Api en express'
+              description='Creación de API con express'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={reactRouterRedux}
+              title='React Router/Redux'
+              description='Routeo con React'
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <CertCard
+              imgPath={reactHook}
+              title='React Hook'
+              description='Manejo de los Hook en React'
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Certs;
