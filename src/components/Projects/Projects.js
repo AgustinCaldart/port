0e7949f623a44a24e3a7cbeb6ba5253b41798bf5
acaldart <caldart123@gmail.com>
2021-12-01 14:16:28 -0300

    arreglos

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
@@ -89,3 +89,4 @@ function Projects() {
 }
 
 export default Projects;
+ */
